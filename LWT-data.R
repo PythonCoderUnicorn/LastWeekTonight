ca1c1ad8889e6dc50cb068007dd69b4a361dd65a
Zane Dax <neptunium0237@gmail.com>
2023-04-20 08:26:05 -0600

    del ( )

---
+++ LWT-data.R
@@ -0,0 +1,12 @@
+
+library(tidyverse)
+library(ggforce)
+library(lubridate)
+library(showtext)
+library(paletteer)
+library(ggtext)
+
+# font files on the local machine.
+theme_set(theme_bw(base_family = "Raleway"))
+
+
